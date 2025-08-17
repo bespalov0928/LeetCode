a32ba5131caf015a17829c0af6b50f902f4dd747
Dmitriy.Bespalov <bespalov0928@mail.ru>
2025-08-17 11:17:47 +0300

    121_BestTimeToBuyAndSellStock

--- 121_BestTimeToBuyAndSellStock/src/main/java/Solution.java
+++ 121_BestTimeToBuyAndSellStock/src/main/java/Solution.java
@@ -8,4 +8,76 @@ public class Solution {
         }
         return profit;
     }
+
+    private int INF = Integer.MAX_VALUE;
+
+    public int maxProfit_var2(int[] prices) {
+//        var cache = new int[prices.length][1];
+//        var rsl = test(prices, 0);
+//        var rsl = 0;
+        var cache = initCash(prices.length);
+        var rsl = run(prices, 0, 0, cache);
+        return rsl;
+    }
+
+    private int test(int[] prices, int index) {
+//        cache[index][0] = prices[index];
+//        System.out.println(prices[index]);
+        if (index == prices.length - 1) {
+            return prices[index];
+        }
+        var maxVal = Math.min(prices[index], test(prices, index + 1));
+//        System.out.println(x);
+//        return -1;
+        return maxVal;
+    }
+
+    private int[][] initCash(int N) {
+        var ans = new int[N][];
+        for (int i = 0; i < N; i++) {
+            ans[i] = new int[]{-1, -1};
+        }
+        return ans;
+    }
+
+    private int run(int[] prices, int idx, int seen, int[][] cache) {
+        int N = prices.length;
+
+        //купили и не продали
+        if (idx == N && seen == 1) {
+//            return prices[idx];
+            return -INF;
+        }
+
+        //не купили и не продали
+        if (idx == N && seen == 0) {
+            return 0;
+        }
+
+        //кэш
+        if (cache[idx][seen] != -1) {
+            return cache[idx][seen];
+        }
+
+        //если мы где-то купили то можем продать
+        if (seen == 1) {
+//            var x1 = prices[idx];
+//            var x2 = run(prices, idx + 1, seen, cache);
+            //продаю или дальше иду
+//            cache[idx][seen] = Math.max(x1, x2);
+            cache[idx][seen] = Math.max(prices[idx], run(prices, idx + 1, seen, cache));
+            return cache[idx][seen];
+        }
+//        var z1 = -prices[idx];
+//        //уже видел операцию покупки seen = true
+//        var z2 = run(prices, idx + 1, 1, cache);
+//        //иду дальше смотрю что купить seen = false
+//        var z3 = run(prices, idx + 1, 0, cache);
+        ////покупаю или дальше иду
+//        cache[idx][seen] = Math.max(-z1 + z2, z3);
+        cache[idx][seen] = Math.max(-prices[idx]
+                        + run(prices, idx + 1, 1, cache)
+                , run(prices, idx + 1, 0, cache));
+        return cache[idx][seen];
+    }
 }
--- 121_BestTimeToBuyAndSellStock/src/test/java/SolutionTest.java
+++ 121_BestTimeToBuyAndSellStock/src/test/java/SolutionTest.java
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
     @org.junit.jupiter.api.Test
-    void maxProfit() {
-
+    void maxProfit_var2() {
+        int actual = new Solution().maxProfit_var2(new int[]{7,1,5,3,6,4});
+        Assertions.assertEquals(5, actual);
     }
 }
